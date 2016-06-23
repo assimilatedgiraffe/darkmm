5118ddcac017f9d2b112843057005abea5fce46d
Benjamin <assimilatedgiraffe@gmail.com>
2016-06-24 00:37:37 +0100

    basic nodes and circle pos calculations

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+from math import pi, cos, sin
+
 from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
 from kivy.uix.textinput import *
@@ -16,56 +18,76 @@ import xml.etree.ElementTree as etree
 
 # parse xml from file
 fullMapTree = etree.parse("test.mm")
-parentMap = dict((c, p) for p in fullMapTree.getiterator() for c in p) #map of parents
+parentMap = dict((c, p) for p in fullMapTree.getiterator() for c in p)  # map of parents
 # warning: destructive, removes formatting
 for el in fullMapTree.getiterator():
     if el.tag != 'node':
         parent = parentMap.get(el)
         if parent is not None:
             parent.remove(el)
-selectedParent = fullMapTree.getroot()[0] # parent of currently selected node
+selectedParent = fullMapTree.getroot()[0]  # parent of currently selected node
 selectedIndex = 0  # currently selected node
 selectedIndexList = [0, 0]
+
+
 # print [El.get("TEXT") for El in mapRootNode]
 
 
 
 class PathListView(StackLayout):
     """path list at top of window like ubuntu file manager"""
+
     def __init__(self, **kwargs):
         super(PathListView, self).__init__(**kwargs)
         child = fullMapTree.getroot()
         for i in selectedIndexList:
             child = child[i]
             btn = Button(text=child.get("TEXT"),
-                                   size_hint=(.15, 1),
-                                   text_size=self.size,
-                                   max_lines=3,
+                         size_hint=(.15, 1),
+                         text_size=self.size,
+                         max_lines=3,
                          halign='left',
                          valign='middle')
             btn.bind(size=btn.setter('text_size'))
             self.add_widget(btn)
 
 
-
-
-
-class Node(TextInput):
+class Node(Button):
     """node for mindmap view"""
+
     def __init__(self, **kwargs):
         super(Node, self).__init__(**kwargs)
-        self.size = (100, 100)
+        # self.size = (100, 100)
+        self.size_hint = (None, None)
+        self.text_size = (150, None)
+        self.halign = 'center'
 
 
-class MindMap(Widget):
+class MindMapView(RelativeLayout):
     """mindmap view"""
+
     def __init__(self, **kwargs):
-        super(MindMap, self).__init__(**kwargs)
-        nodes = [Node(), Node(), Node()]
-        for i, node in enumerate(nodes):
-            node.text = 'node ' + str(i)
-            node.pos[0] = node.pos[0] + (i*100)
+        super(MindMapView, self).__init__(**kwargs)
+        # selected node in centre
+        center_node = Node()
+        center_node.text = selectedParent[selectedIndexList[-1]].get('TEXT')
+        center_node.size = center_node.texture_size
+        center_node.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        self.add_widget(center_node)
+
+        # circle of child nodes
+        n_nodes = len(selectedParent[selectedIndexList[-1]])
+        for i, child in enumerate(selectedParent[selectedIndexList[-1]]):
+            node = Node()
+            node.text = child.get('TEXT')
+            node.size = node.texture_size
+            # calculate pos
+            r = .3
+            w = i * (2 * pi / n_nodes)
+            x, y = (r * cos(w)) + .5, (r * sin(w)) + .5
+            node.pos_hint = {'center_x': x, 'center_y': y}
             self.add_widget(node)
+
 
 class TreeView(BoxLayout):
     """ Main middle col, parent selected n uncles left, child nodes on right """
@@ -97,17 +119,15 @@ class TreeView(BoxLayout):
         self.add_widget(ListView(adapter=child_list_adapter))
 
 
-
-
-
 class OverView(BoxLayout):
     """main window"""
+
     def __init__(self, **kwargs):
         super(OverView, self).__init__(**kwargs)
 
         self.orientation = 'vertical'
         self.add_widget(PathListView(size_hint=(1, .1)))
-        self.add_widget(TreeView(size_hint=(1, .9)))
+        self.add_widget(MindMapView(size_hint=(1, .9)))
 
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
@@ -129,10 +149,10 @@ class OverView(BoxLayout):
             if len(selectedParent) > selectedIndexList[-1] + 1:
                 selectedIndexList[-1] += 1
                 self.refresh_UI()
-#            index = self.main_list_adapter.selection[0].index + 1
-#            new_selection = self.main_list_adapter.get_view(index)
-#            if new_selection and not new_selection.is_selected:
-#                new_selection.trigger_action(duration=0)
+            #            index = self.main_list_adapter.selection[0].index + 1
+            #            new_selection = self.main_list_adapter.get_view(index)
+            #            if new_selection and not new_selection.is_selected:
+            #                new_selection.trigger_action(duration=0)
         elif keycode[1] == 'k':
             if selectedIndexList[-1] > 0:
                 selectedIndexList[-1] -= 1
@@ -150,11 +170,9 @@ class OverView(BoxLayout):
         return True
 
 
-
 class MindMapApp(App):
-
     def loadFromFile(self):
-        #parse xml from file
+        # parse xml from file
         tree = etree.parse("test.mm")
         mmap = tree.getroot()[0]
 
